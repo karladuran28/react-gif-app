4d7e0d3f79c53b7a04ceedc65c82dc1e4a896ed3
karladuran28 <kaduran1998@gmail.com>
2021-08-21 23:27:42 -0500

    uploading project

---
+++ src/componentes/GifExpertApp.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import AddCategory from './AddCategory';
+import GifGrid from './GifGrid';
+
+/* test comment */
+
+const GifExpertApp = () => {
+
+    const [categorias, setCategorias] = useState(['Techno']);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategorias={ setCategorias }/>
+            <hr />
+
+            <ol>
+                {
+                    categorias.map( categoria => (
+                        <GifGrid
+                            key={ categoria }
+                            categoria={ categoria } 
+                        /> 
+                    ))
+                }
+            </ol>
+        </>
+    )
+};
+
+export default GifExpertApp
